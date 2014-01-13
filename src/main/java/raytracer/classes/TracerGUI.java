7f83e173ebb68f5a0972780620a70c8bc2333383
coffeMug <aminkhorsandi@gmail.com>
2014-01-13 21:28:06 +0100

    Some small changes. Added a button in GUI.

--- src/main/java/raytracer/classes/TracerGUI.java
+++ src/main/java/raytracer/classes/TracerGUI.java
@@ -1,27 +1,41 @@
 package classes;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TracerGUI extends JFrame implements ActionListener {
 
-	public TracerGUI() {
-		init();
-	}
+    JPanel compsPanel;
+    JButton chooseFileButton; 
 
-	private void init(){
-		this.setSize(500, 300); 
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-	}
+    public TracerGUI() {
+        init_comps_panel();
+        init_choose_file_button();
+        this.setSize(500, 300); 
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 
+    private void init_comps_panel() {
+        compsPanel = new JPanel();
+        this.add(compsPanel, BorderLayout.WEST);
+    }
+
+    private void init_choose_file_button() {
+	chooseFileButton= new JButton("Choose a ppm file...");
+	chooseFileButton.setVerticalTextPosition(AbstractButton.CENTER);
+	chooseFileButton.setHorizontalTextPosition(AbstractButton.LEADING);
+	chooseFileButton.setPreferredSize(new Dimension(100, 30));
+        this.compsPanel.add(chooseFileButton);
+    }
 
     public void actionPerformed(ActionEvent e) {
-		// if (e.getActionCommand() == "start"){
-		// 	board.start();
-		// }
+        // if (e.getActionCommand() == "start"){
+        //  board.start();
+        // }
     }
-	
+    
 }
