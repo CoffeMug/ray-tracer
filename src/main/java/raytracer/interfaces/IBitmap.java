66150574a0f1fb13ad26f6df2711f8e03c0de476
coffeMug <aminkhorsandi@gmail.com>
2014-01-22 20:33:24 +0100

    Indentation fixes for interfaces.

--- src/main/java/raytracer/interfaces/IBitmap.java
+++ src/main/java/raytracer/interfaces/IBitmap.java
@@ -11,48 +11,45 @@ import classes.Color;
  *
  */
 public interface IBitmap {
-
-	/**
-	 * returns bitmap width.
-	 * @return an integer representing bitmap width.
-	 */
-	int getWidth();
-	
-	/**
-	 * returns bitmap height.
-	 * @return an integer representing bitmap height.
-	 */
-	int getHeight();
-	
-	/**
-	 * this method gets x and y coordinates of a single pixel in our bitmap
-	 * objects and returns the color of that pixel.
-	 * @param xCord X coordinate of the pixel in bitmap matrix.
-	 * @param yCord Y coordinate of the pixel in bitmap matrix.
-	 * @return color of the pixel in (x,y) coordinate.
-	 */
-	 Color getSinglePixel (int xCord, int yCord) throws Exception;
-	
-	/**
-	 * bitmap class uses this method locally to write its 2-D pixel array
-	 * to a corresponding PPM file.
-	 * @param variant type of PPM file we are going to write to. it can be
-	 * either ASCII or BINARY.
-	 * @param filePath path to the file we are going to write to.
-	 * @return true if pixels are written to file successfully and false if
-	 * not.
-	 */
-	boolean writeBitmapToFile(int variant, String filePath);
-	
-	/**
-	 * this method searches for a pixel in our bitmap ADT having pixel's
-	 * X and Y coordinates. then given a color c, it will change pixel's
-	 * color to c. 
-	 * @param xCord x coordinate of pixel we are going to change its color.
-	 * @param yCord y coordinate of pixel we are going to change its color.
-	 * @param colr instance of class Color
-	 */
-	void setPixel(int xCord, int yCord, Color colr);
-	
-	
+    /**
+     * returns bitmap width.
+     * @return an integer representing bitmap width.
+     */
+    int getWidth();
+        
+    /**
+     * returns bitmap height.
+     * @return an integer representing bitmap height.
+     */
+    int getHeight();
+        
+    /**
+     * this method gets x and y coordinates of a single pixel in our bitmap
+     * objects and returns the color of that pixel.
+     * @param xCord X coordinate of the pixel in bitmap matrix.
+     * @param yCord Y coordinate of the pixel in bitmap matrix.
+     * @return color of the pixel in (x,y) coordinate.
+     */
+    Color getSinglePixel (int xCord, int yCord) throws Exception;
+        
+    /**
+     * bitmap class uses this method locally to write its 2-D pixel array
+     * to a corresponding PPM file.
+     * @param variant type of PPM file we are going to write to. it can be
+     * either ASCII or BINARY.
+     * @param filePath path to the file we are going to write to.
+     * @return true if pixels are written to file successfully and false if
+     * not.
+     */
+    boolean writeBitmapToFile(int variant, String filePath);
+        
+    /**
+     * this method searches for a pixel in our bitmap ADT having pixel's
+     * X and Y coordinates. then given a color c, it will change pixel's
+     * color to c. 
+     * @param xCord x coordinate of pixel we are going to change its color.
+     * @param yCord y coordinate of pixel we are going to change its color.
+     * @param colr instance of class Color
+     */
+    void setPixel(int xCord, int yCord, Color colr);
 }
--- src/main/java/raytracer/interfaces/IMaterial.java
+++ src/main/java/raytracer/interfaces/IMaterial.java
@@ -10,21 +10,20 @@ import classes.Color;
  *
  */
 public interface IMaterial {
-	
-	/**
-	 * this method checks if current material is of type solid or texture.
-	 * @return true as texture or false as solid.
-	 */
-	boolean hasTexture(); 
-	
-	/**
-	 * gets a coordinate on shape and returns color of that coordinate. 
-	 * @param uDbl
-	 * @param vDbl
-	 * @return color of the pixel located on that coordinate.
-	 */
-	Color getColor(double uDbl, double vDbl);
-	void setReflection(double ref);
-	double getReflection();
-	double getDiffuse();
+    /**
+     * this method checks if current material is of type solid or texture.
+     * @return true as texture or false as solid.
+     */
+    boolean hasTexture(); 
+        
+    /**
+     * gets a coordinate on shape and returns color of that coordinate. 
+     * @param uDbl
+     * @param vDbl
+     * @return color of the pixel located on that coordinate.
+     */
+    Color getColor(double uDbl, double vDbl);
+    void setReflection(double ref);
+    double getReflection();
+    double getDiffuse();
 }
