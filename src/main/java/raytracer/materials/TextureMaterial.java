d6a25dba92b8d9966754af82b4f29cf95bad9a41
coffeMug <aminkhorsandi@gmail.com>
2014-01-23 19:38:10 +0100

    Improved indentation in material classes.

--- src/main/java/raytracer/materials/TextureMaterial.java
+++ src/main/java/raytracer/materials/TextureMaterial.java
@@ -13,45 +13,40 @@ import classes.Color;
  */
 public class TextureMaterial extends BaseMaterial {
 
-	 public transient Texture texture;
+    public transient Texture texture;
      
-	 /**
-	  * this constructor gets texture object a texture material should 
-	  * represent and the reflection coefficient of material
-	  * @param text texture object.
-	  * @param reflection amount of reflection the texture should implement.
-	  */
-     public TextureMaterial(final Texture text, final double reflection)
-     {
-    	 super(); // used for suppressing PMD warning
-         this.reflection = reflection;
-         this.texture = text;
-     }
+    /**
+     * this constructor gets texture object a texture material should 
+     * represent and the reflection coefficient of material
+     * @param text texture object.
+     * @param reflection amount of reflection the texture should implement.
+     */
+    public TextureMaterial(final Texture text, final double reflection)
+    {
+        super(); // used for suppressing PMD warning
+        this.reflection = reflection;
+        this.texture = text;
+    }
 
-     @Override  public  boolean hasTexture() {
-         return true;
-         }
-   
+    @Override public boolean hasTexture() {
+        return true;
+    }
 
-     /**
-      * using texture class , this method gets color of a specified color on 
-      * the shape in vector coordinates, according to its corresponding color
-      * on a texture object in X,Y coordinates.
-      */
-     @Override    public  Color getColor(final double ui , final double vi){
+    /**
+     * using texture class, this method gets color of a specified color on 
+     * the shape in vector coordinates, according to its corresponding color
+     * on a texture object in X,Y coordinates.
+     */
+    @Override public Color getColor(final double ui, final double vi){
+        double uu = (ui)  * texture.width;
+        double vv = (1-vi) * texture.height;
+        Color c1 = texture.colorMap[(int)uu][(int)vv];
+
+        return c1;
+    }
      
-    	 	double uu = (ui)  * texture.width;
-    		double vv = (1-vi) * texture.height;
-  
-    		Color c1 = texture.colorMap[(int)uu][(int)vv];
-  
-    		return c1;
-
-         
-     }
-     
-     @Override    public  double getDiffuse(){
- 		return 1.0;
- 	}
- }
+    @Override    public  double getDiffuse(){
+        return 1.0;
+    }
+}
 
